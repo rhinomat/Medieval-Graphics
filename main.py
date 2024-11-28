9075a962a82956a940182887f6e4541b608d86cb
Ryan <etrigan976@gmail.com>
2024-11-28 14:07:06 -0800

    Cleanup: Deleted functions from main.py that were moved to obj.py

--- main.py
+++ main.py
@@ -5,26 +5,6 @@ from OpenGL.GLU import *
 from PIL import Image
 import math
 import obj
-def load_obj(file_path):
-    vertices = []
-    faces = []
-    texture_coords = []
-
-    with open(file_path, 'r') as f:
-        for line in f:
-            if line.startswith('v '):
-                parts = line.split()
-                vertices.append((float(parts[1]), float(parts[2]), float(parts[3])))
-            elif line.startswith('vt '):
-                parts = line.split()
-                texture_coords.append((float(parts[1]), float(parts[2])))
-            elif line.startswith('f '):
-                parts = line.split()
-                face = [int(part.split('/')[0]) - 1 for part in parts[1:]]
-                tex_face = [int(part.split('/')[1]) - 1 for part in parts[1:]]
-                faces.append((face, tex_face))
-
-    return vertices, faces, texture_coords
 
 def init_opengl(screen_width, screen_height):
     glEnable(GL_DEPTH_TEST)
@@ -34,44 +14,6 @@ def init_opengl(screen_width, screen_height):
     glMatrixMode(GL_PROJECTION)
     gluPerspective(45, (screen_width / screen_height), 0.1, 100.0)
     glMatrixMode(GL_MODELVIEW)
-
-def modify_texture_coords(texture_coords, scale=2.0):
-    # Scale texture coordinates to make the texture repeat
-    modified_coords = [(u * scale, v * scale) for u, v in texture_coords]
-    return modified_coords
-
-def load_tga_texture(texture_path):
-    image = Image.open(texture_path)
-    image = image.convert("RGB")
-    
-    width, height = image.size
-    image_data = image.tobytes("raw", "RGB", 0, -1)
-
-    glEnable(GL_TEXTURE_2D)
-    texture_id = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, texture_id)
-
-    # Set texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)  # Repeat horizontally
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)  # Repeat vertically
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
-
-    return texture_id
-
-def draw_model(vertices, faces, texture_coords, texture_id):
-    # Bind texture
-    glBindTexture(GL_TEXTURE_2D, texture_id)
-
-    # Draw model with texture
-    glBegin(GL_TRIANGLES)
-    for face, tex_face in faces:
-        for i, vertex_idx in enumerate(face):
-            glTexCoord2fv(texture_coords[tex_face[i]])
-            glVertex3fv(vertices[vertex_idx])
-    glEnd()
 
 def main():
     screen_width = 800
@@ -83,6 +25,7 @@ def main():
 
     object_1 = obj.object()
     object_1.load_file('MainPlatform.obj')
+    object_1.scale(2, 1, 2);
     object_1.scale_texture(20.0)
     object_1.load_texture('grass.tga')
 
